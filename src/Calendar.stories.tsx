0e867fa46fece3ab886e2587c0d0c8479d7caedf
gosho-kazuya <ketsume0211@gmail.com>
2020-02-14 18:08:06 +0900

    add first storybook

--- src/Calendar.stories.tsx
+++ src/Calendar.stories.tsx
@@ -1,12 +1,22 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { Dimensions, View, StyleSheet } from 'react-native'
+import dayjs from 'dayjs'
 import { Calendar } from './Calendar'
 
+const MOBILE_HEIGHT = 736
+
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f8f8f8',
+  desktop: {
     height: '100%',
+  },
+  mobile: {
+    width: 414,
+    height: MOBILE_HEIGHT,
+    overflow: 'hidden',
+    borderWidth: 10,
+    borderRadius: 10,
+    boxSizing: 'content-box',
   },
   calendar: {
     borderWidth: 1,
@@ -18,28 +28,71 @@ const styles = StyleSheet.create({
 const events = [
   {
     title: 'Meeting',
-    start: new Date(2020, 1, 13, 10, 0),
-    end: new Date(2020, 1, 13, 10, 30),
+    start: dayjs()
+      .set('hour', 10)
+      .set('minute', 0)
+      .toDate(),
+    end: dayjs()
+      .set('hour', 10)
+      .set('minute', 30)
+      .toDate(),
   },
   {
     title: 'Coffee break',
-    start: new Date(2020, 1, 14, 15, 45),
-    end: new Date(2020, 1, 14, 16, 30),
+    start: dayjs()
+      .set('hour', 14)
+      .set('minute', 30)
+      .toDate(),
+    end: dayjs()
+      .set('hour', 15)
+      .set('minute', 30)
+      .toDate(),
   },
   {
     title: 'Repair my car',
-    start: new Date(2020, 1, 16, 7, 45),
-    end: new Date(2020, 1, 16, 13, 30),
+    start: dayjs()
+      .add(1, 'day')
+      .set('hour', 7)
+      .set('minute', 45)
+      .toDate(),
+    end: dayjs()
+      .add(1, 'day')
+      .set('hour', 13)
+      .set('minute', 30)
+      .toDate(),
   },
 ]
 
-storiesOf('Calendar', module).add('main', () => (
-  <View style={styles.container}>
-    <Calendar
-      style={styles.calendar}
-      height={Dimensions.get('window').height}
-      events={events}
-      mode="3days"
-    />
-  </View>
-))
+storiesOf('Desktop', module)
+  .add('3days mode', () => (
+    <View style={styles.desktop}>
+      <Calendar
+        style={styles.calendar}
+        height={Dimensions.get('window').height}
+        events={events}
+        mode="3days"
+      />
+    </View>
+  ))
+  .add('week mode', () => (
+    <View style={styles.desktop}>
+      <Calendar
+        style={styles.calendar}
+        height={Dimensions.get('window').height}
+        events={events}
+        mode="week"
+      />
+    </View>
+  ))
+
+storiesOf('Mobile', module)
+  .add('3days mode', () => (
+    <View style={styles.mobile}>
+      <Calendar style={styles.calendar} height={MOBILE_HEIGHT} events={events} mode="3days" />
+    </View>
+  ))
+  .add('week mode', () => (
+    <View style={styles.mobile}>
+      <Calendar style={styles.calendar} height={MOBILE_HEIGHT} events={events} mode="week" />
+    </View>
+  ))
