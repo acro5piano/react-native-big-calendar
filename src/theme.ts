1ab169c139917762e716f474c361a8cc60a99cbf
Shawn Roller <sroller@techstyle.com>
2020-09-15 15:05:10 -0400

    Updates readme and theme colors.

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,8 @@
 export enum Color {
   primary = 'rgb(66, 133, 244)',
-  red = 'red',
-  yellow = 'yellow',
-  green = 'green',
-  orange = 'orange',
-  pink = 'pink',
+  red = '#C80B22',
+  yellow = '#F8E71C',
+  green = '#4AC001',
+  orange = '#E26245',
+  pink = '#5934C7',
 }
