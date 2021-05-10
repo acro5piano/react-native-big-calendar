43ea30acd1cb5088073eae8fa4f75a12e203628b
gosho-kazuya <ketsume0211@gmail.com>
2021-05-10 09:57:13 +0900

    remove old example as for storybook, update docs

--- example/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import App from './App'
-import { AppRegistry } from 'react-native'
-
-AppRegistry.registerComponent('App', () => App)
-AppRegistry.runApplication('App', { rootTag: document.getElementById('root') })
