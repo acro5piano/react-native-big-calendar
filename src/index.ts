6d2811b07e295b342d86859d1013df78e875b994
Omkar.Jadhav <83406048+mindbodysolotestfirebase@users.noreply.github.com>
2021-07-30 14:18:28 +0530

    Exported CalendarBody component
    For one of use case we have to just use CalendarBody component. But it was not accessible as we haven't exported it. Made changes related to it
    Tested locally.
    Thank you

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Calendar } from './components/Calendar'
 export * from './components/Calendar'
 export * from './components/DefaultCalendarEventRenderer'
 export * from './interfaces'
+export * from './components/CalendarBody'
 
 /* eslint-disable */
 export default Calendar
