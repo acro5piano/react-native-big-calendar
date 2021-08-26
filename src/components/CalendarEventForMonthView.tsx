b85181a5c88e8f01c35571c414941f4149864772
Etienne Maurin <etienne@collab.ee>
2021-08-26 18:58:25 +0900

    update month view event UI to handle spanning over multiple days

--- src/components/CalendarEventForMonthView.tsx
+++ src/components/CalendarEventForMonthView.tsx
@@ -1,5 +1,6 @@
+import dayjs from 'dayjs'
 import * as React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { u } from '../commonStyles'
 import { useCalendarTouchableOpacityProps } from '../hooks/useCalendarTouchableOpacityProps'
@@ -12,6 +13,8 @@ interface CalendarEventProps<T> {
   onPressEvent?: (event: ICalendarEvent<T>) => void
   eventCellStyle?: EventCellStyle<T>
   renderEvent?: EventRenderer<T>
+  date: dayjs.Dayjs
+  dayOfTheWeek: number
 }
 
 function _CalendarEventForMonthView<T>({
@@ -19,14 +22,38 @@ function _CalendarEventForMonthView<T>({
   onPressEvent,
   eventCellStyle,
   renderEvent,
+  date,
+  dayOfTheWeek,
 }: CalendarEventProps<T>) {
   const theme = useTheme()
+
+  // adding + 1 because durations start at 0
+  const eventDuration = dayjs.duration(dayjs(event.end).diff(dayjs(event.start))).days() + 1
+  const eventDaysLeft = dayjs.duration(dayjs(event.end).diff(date)).days() + 1
+  const weekDaysLeft = 7 - dayOfTheWeek
+  //
+  const eventWeekDuration =
+    eventDuration > weekDaysLeft
+      ? weekDaysLeft
+      : dayOfTheWeek === 0 && eventDaysLeft < eventDuration
+      ? eventDaysLeft
+      : eventDuration
+  const isMultipleDays = eventDuration > 1
+  const isMultipleDaysStart =
+    isMultipleDays &&
+    (date.isSame(event.start, 'day') || (dayOfTheWeek === 0 && date.isAfter(event.start)))
 
   const touchableOpacityProps = useCalendarTouchableOpacityProps({
     event,
     eventCellStyle,
     onPressEvent,
-    injectedStyles: [{ backgroundColor: theme.palette.primary.main }, u['mt-2']],
+    injectedStyles: [
+      { backgroundColor: theme.palette.primary.main },
+      isMultipleDaysStart
+        ? { position: 'absolute', width: `${eventWeekDuration * 100}%`, zIndex: 100000 }
+        : {},
+      u['mt-2'],
+    ],
   })
 
   if (renderEvent) {
@@ -34,14 +61,23 @@ function _CalendarEventForMonthView<T>({
   }
 
   return (
-    <TouchableOpacity {...touchableOpacityProps}>
-      <Text
-        style={[{ color: theme.palette.primary.contrastText }, theme.typography.xs, u['truncate']]}
-        numberOfLines={1}
-      >
-        {event.title}
-      </Text>
-    </TouchableOpacity>
+    <View style={{ minHeight: 22 }}>
+      {((!isMultipleDays && date.isSame(event.start, 'day')) ||
+        (isMultipleDays && isMultipleDaysStart)) && (
+        <TouchableOpacity {...touchableOpacityProps}>
+          <Text
+            style={[
+              { color: theme.palette.primary.contrastText },
+              theme.typography.xs,
+              u['truncate'],
+            ]}
+            numberOfLines={1}
+          >
+            {event.title}
+          </Text>
+        </TouchableOpacity>
+      )}
+    </View>
   )
 }
 
