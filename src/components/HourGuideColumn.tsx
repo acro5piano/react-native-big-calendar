f94a9c7d8085106471ae48c3f28d9fb94b56fe62
Rafael Mora <rafael.mora.guti@gmail.com>
2021-11-24 10:10:24 -0500

    Improve code for the isPair function

--- src/components/HourGuideColumn.tsx
+++ src/components/HourGuideColumn.tsx
@@ -12,18 +12,19 @@ interface HourGuideColumnProps {
   index: number
 }
 
+const isPair = (i: number) => i % 2
+
 const _HourGuideColumn = ({ cellHeight, hour, ampm, index }: HourGuideColumnProps) => {
   const theme = useTheme()
   const textStyle = React.useMemo(
     () => ({ color: theme.palette.gray[500], fontSize: theme.typography.xs.fontSize }),
     [theme],
   )
-  const is_pair = (i) => i % 2
   const pairCellBg = { backgroundColor: theme.palette.primary.pairCellBg }
   const oddCellBg = { backgroundColor: theme.palette.primary.oddCellBg }
 
   return (
-    <View style={{ height: cellHeight, backgroundColor: is_pair(index) ? pairCellBg : oddCellBg }}>
+    <View style={{ height: cellHeight, backgroundColor: isPair(index) ? pairCellBg : oddCellBg }}>
       <Text style={[textStyle, u['text-center']]}>{formatHour(hour, ampm)}</Text>
     </View>
   )
