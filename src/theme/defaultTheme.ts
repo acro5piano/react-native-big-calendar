1ade38e9c724436a6eacc421895f7b5853bd4f67
Rafael Mora <rafael.mora.guti@gmail.com>
2021-11-24 22:27:46 -0500

    Add missing even and odd cell bg to the default Theme

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -7,7 +7,7 @@ export const defaultTheme: ThemeInterface = {
       main: 'rgb(66, 133, 244)',
       contrastText: '#fff',
       evenCellBg: '#fff',
-      oddCellBg: '#fff'
+      oddCellBg: '#fff',
     },
     nowIndicator: 'red',
     gray: {
@@ -24,9 +24,9 @@ export const defaultTheme: ThemeInterface = {
     },
   },
   eventCellOverlappings: [
-    { main: '#E26245', contrastText: '#fff' }, // orange
-    { main: '#4AC001', contrastText: '#fff' }, // green
-    { main: '#5934C7', contrastText: '#fff' }, // purple
+    { main: '#E26245', contrastText: '#fff', evenCellBg: '#fff', oddCellBg: '#fff' }, // orange
+    { main: '#4AC001', contrastText: '#fff', evenCellBg: '#fff', oddCellBg: '#fff' }, // green
+    { main: '#5934C7', contrastText: '#fff', evenCellBg: '#fff', oddCellBg: '#fff' }, // purple
   ],
   typography: {
     xs: {
