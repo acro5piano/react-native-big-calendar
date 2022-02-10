c70235660feccc5cb3ecad755bd1da890205be5c
ghoudi <ghoudi31@gmail.com>
2022-02-10 10:52:49 +0100

    fix: files permission from 100755 to 100644

--- rndemo/App.tsx
+++ rndemo/App.tsx
@@ -15,11 +15,7 @@ const events = [
     start: dayjs().set('hour', 14).set('minute', 30).toDate(),
     end: dayjs().set('hour', 15).set('minute', 30).toDate(),
   },
-  {
-    title: 'Repair my car',
-    start: dayjs().add(1, 'day').set('hour', 7).set('minute', 45).toDate(),
-    end: dayjs().add(1, 'day').set('hour', 13).set('minute', 30).toDate(),
-  },
+ 
 ]
 
 export const App = () => {
@@ -32,7 +28,7 @@ export const App = () => {
       const end = dayjs(start).add(59, 'minute').toDate()
       setAdditionalEvents([...additionalEvents, { start, end, title }])
     },
-    
+
     [additionalEvents, setAdditionalEvents],
   )
 
