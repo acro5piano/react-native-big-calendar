ca2e00cea632757a43f0eabe32ad4cf6e8e4ffb1
Fabrian Ibrahim <fabrian.ibrahim@gmail.com>
2022-07-14 20:07:36 +1000

    fix key warning

--- src/hooks/useCalendarTouchableOpacityProps.ts
+++ src/hooks/useCalendarTouchableOpacityProps.ts
@@ -38,7 +38,7 @@ export function useCalendarTouchableOpacityProps<T extends ICalendarEventBase>({
 
   const touchableOpacityProps: CalendarTouchableOpacityProps = {
     delayPressIn: 20,
-    key: event.start.toString(),
+    key: `${event.start.toISOString()}_${event.title}`,
     style: [eventCellCss.style, ...injectedStyles, getEventStyle(plainJsEvent)],
     onPress: _onPress,
     disabled: !onPressEvent,
