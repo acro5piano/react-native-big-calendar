d62d4b345821034e7d2fe5c1b164142c7d01c929
charithAmila <dkcadissanayaka@gmail.com>
2023-10-16 13:59:34 -0400

    added FlatList component and group events by date

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import dayjs from 'dayjs'
+import { FlatList, TextStyle, View, ViewStyle } from 'react-native'
 import { typedMemo } from '../utils/react'
-import { Text, View } from 'react-native'
+import {
+  CalendarCellStyle,
+  EventCellStyle,
+  EventRenderer,
+  HorizontalDirection,
+  ICalendarEventBase,
+} from '../interfaces'
 
-function _Schedule() {
+interface ScheduleProps<T extends ICalendarEventBase> {
+  events: T[]
+  cellHeight: number
+  containerHeight: number
+  dateRange: dayjs.Dayjs[]
+  scrollOffsetMinutes: number
+  ampm: boolean
+  showTime: boolean
+  style: ViewStyle
+  eventCellStyle?: EventCellStyle<T>
+  calendarCellStyle?: CalendarCellStyle
+  hideNowIndicator?: boolean
+  overlapOffset?: number
+  onLongPressCell?: (date: Date) => void
+  onPressCell?: (date: Date) => void
+  onPressEvent?: (event: T) => void
+  onSwipeHorizontal?: (d: HorizontalDirection) => void
+  renderEvent?: EventRenderer<T>
+  headerComponent?: React.ReactElement | null
+  headerComponentStyle?: ViewStyle
+  hourStyle?: TextStyle
+  hideHours?: Boolean
+  isEventOrderingEnabled?: boolean
+  showVerticalScrollIndicator?: boolean
+}
+
+function _Schedule<T extends ICalendarEventBase>({ events }: ScheduleProps<T>) {
+  const renderFlatListItem = (item: T[]) => {
+    console.log(item)
+    return <></>
+  }
+
+  const getItem = useMemo(() => {
+    const groupedData = events.reduce((result: any, item: T): any => {
+      const startDate = dayjs(item.start).format('YYYY-MM-DD')
+      if (!result[startDate]) {
+        result[startDate] = []
+      }
+      result[startDate].push(item)
+      return result
+    }, {})
+
+    return Object.values(groupedData)
+  }, [events])
+
   return (
     <View>
-      <Text>Schedule view</Text>
+      <FlatList data={getItem} renderItem={({ item }: { item: any }) => renderFlatListItem(item)} />
     </View>
   )
 }
